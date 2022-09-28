3c749a8e8f8a6ae6cd3d90c99865132d9533e76e
Artem <artemkhom4enko@gmail.com>
2022-09-28 10:40:24 +0300

    hw task final ver.1.0  done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,22 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {goods: [], order: {}, client: ""};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+    data.client = name + ' ' + phone;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat;
+    data.order.sum = sum;
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    for (let i = 0; i < countOfGoods; i ++) {
+        let good = {};
+        good.title = goods[i].title;
+        good.count = goods[i].count;
+        data.goods.push(good);
+   }
+   
+   let resultJson = {data: data}
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
+   return JSON.stringify(resultJson);
 }
